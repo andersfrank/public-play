610e49ae6687ad6f5459c40d595898285d83019b
Anders Frank <anders.frank@ur.se>
2022-03-24 10:58:41 +0100

    Initial comit

---
+++ Public Play/Public_PlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Public_PlayApp.swift
+//  Public Play
+//
+//  Created by Anders Frank on 2022-03-24.
+//
+
+import SwiftUI
+
+@main
+struct Public_PlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
