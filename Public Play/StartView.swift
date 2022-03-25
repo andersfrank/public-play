cac3b0eb8375f0b1f51eb8541e43ab1e13f60bb9
Marcus Linder <marcus.linder@svt.se>
2022-03-25 09:04:42 +0100

    Added padding to startview

--- Public Play/StartView.swift
+++ Public Play/StartView.swift
@@ -53,30 +53,34 @@ struct HList: View {
             Text(title)
                 .font(.title3)
                 .fontWeight(.bold)
+                .padding(.horizontal)
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 10) {
-                    ForEach(programs) { program in
-                        VStack(alignment: .leading) {
-                            AsyncImage(url: program.image, content: { image in image.resizable().aspectRatio(contentMode: .fill).frame(width: size.width, height: size.height) }, placeholder: { ProgressView() })
-                                .frame(width: size.width, height: size.height)
-                                .clipped()
-                                .cornerRadius(4)
-                                .overlay(
-                                    Image(program.provider.image)
-                                        .resizable()
-                                        .frame(width: size.width / 8, height: size.width / 8)
-                                        .cornerRadius(3)
-                                        .offset(x: size.width / 2 - size.width / 11, y: size.height / 2 - size.width / 12)
-                                        .shadow(radius: 3)
-                                ).shadow(radius: 3)
+                VStack {
+                    HStack(spacing: 10) {
+                        ForEach(programs) { program in
+                            VStack(alignment: .leading) {
+                                AsyncImage(url: program.image, content: { image in image.resizable().aspectRatio(contentMode: .fill).frame(width: size.width, height: size.height) }, placeholder: { ProgressView() })
+                                    .frame(width: size.width, height: size.height)
+                                    .clipped()
+                                    .cornerRadius(4)
+                                    .overlay(
+                                        Image(program.provider.image)
+                                            .resizable()
+                                            .frame(width: size.width / 8, height: size.width / 8)
+                                            .cornerRadius(3)
+                                            .offset(x: size.width / 2 - size.width / 11, y: size.height / 2 - size.width / 12)
+                                            .shadow(radius: 3)
+                                    ).shadow(radius: 3)
                                 
-                            HStack {
-                                Text(program.title)
-                                Spacer()
-                            }.frame(width: size.width, height: 20)
+                                HStack {
+                                    Text(program.title)
+                                    Spacer()
+                                }.frame(width: size.width, height: 20)
+                            }
                         }
                     }
                 }
+                .padding(.horizontal)
             }
         }
     }
