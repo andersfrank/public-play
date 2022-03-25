a9d79bef64fbc2bc7c0cc2aceed2a18c801e5556
Anders Frank <anders.frank@ur.se>
2022-03-25 08:14:23 +0100

    Support small items in horizontal list

--- Public Play/StartView.swift
+++ Public Play/StartView.swift
@@ -13,9 +13,17 @@ struct StartView: View {
     var body: some View {
         NavigationView {
             ScrollView(.vertical, showsIndicators: false) {
-                HList(title: "Populärt", programs: popular)
+                HList(
+                    title: "Populärt",
+                    programs: popular,
+                    size: .init(width: 330, height: 185)
+                )
                 Spacer(minLength: 20)
-                HList(title: "Nytt", programs: new)
+                HList(
+                    title: "Nytt",
+                    programs: new,
+                    size: .init(width: 140, height: 78)
+                )
                 Spacer()
             }.navigationTitle("Titta nu")
         }
@@ -31,33 +39,34 @@ struct StartView_Previews: PreviewProvider {
 struct HList: View {
     var title: String
     var programs: [Program]
+    var size: CGSize
 
     var body: some View {
         VStack(alignment: .leading) {
             Text(title)
-                .font(.title2)
+                .font(.title3)
                 .fontWeight(.bold)
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 10) {
                     ForEach(programs) { program in
                         VStack(alignment: .leading) {
-                            AsyncImage(url: program.image, content: { image in image.resizable().aspectRatio(contentMode: .fill).frame(width: 330, height: 185) }, placeholder: { ProgressView() })
-                                .frame(width: 330, height: 185)
+                            AsyncImage(url: program.image, content: { image in image.resizable().aspectRatio(contentMode: .fill).frame(width: size.width, height: size.height) }, placeholder: { ProgressView() })
+                                .frame(width: size.width, height: size.height)
                                 .clipped()
                                 .cornerRadius(4)
                                 .overlay(
                                     Image(program.provider.image)
                                         .resizable()
-                                        .frame(width: 40, height: 40)
+                                        .frame(width: size.width / 8, height: size.width / 8)
                                         .cornerRadius(3)
-                                        .offset(x: 139, y: 67)
+                                        .offset(x: size.width / 2 - size.width / 11, y: size.height / 2 - size.width / 12)
                                         .shadow(radius: 3)
                                 ).shadow(radius: 3)
                                 
                             HStack {
                                 Text(program.title)
                                 Spacer()
-                            }.frame(width: 300, height: 20)
+                            }.frame(width: size.width, height: 20)
                         }
                     }
                 }
