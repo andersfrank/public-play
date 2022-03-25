95fdf71faa3f6551dfa2da684c6727464d5f142a
Marcus Linder <marcus.linder@svt.se>
2022-03-25 12:40:19 +0100

    Redesign content UI for program

--- Public Play/ProgramView.swift
+++ Public Play/ProgramView.swift
@@ -10,57 +10,73 @@ import SwiftUI
 struct ProgramView: View {
     let program: Program
     
-    @State var heightSizeOfImage: CGFloat = UIScreen.main.bounds.height/2
-    
-    @State var weightSizeOfImage: CGFloat = UIScreen.main.bounds.width
-    
     var body: some View {
-        ZStack {
-            VStack(alignment: .leading, spacing: 20) {
-                
+        GeometryReader { geo in
+            ZStack(alignment: .bottom) {
                 AsyncImage(
                     url: program.image,
                     content: { image in
                         image.resizable().aspectRatio( contentMode: .fill) }, placeholder: { ProgressView() }
                 )
-                .frame(width: weightSizeOfImage, height: heightSizeOfImage)
+                .frame(width: geo.size.width, height: geo.size.height)
                 .background(Color("PlaceholderBackground"))
                 .clipped()
                 
-                Text(program.description)
-                    .font(.body)
-                    .multilineTextAlignment(.leading)
-                    .padding(.horizontal)
-                
-                VStack {
-                    HStack(alignment: .center) {
-                        Spacer()
-                        Button() {
-                            openProgram()
-                        } label: {
-                            Label("Spela", systemImage: "play.fill").padding(.horizontal).font(.callout.bold())
-                        }
-                        .buttonStyle(.borderedProminent)
-                        .tint(.accentColor)
-                        
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading) {
+                        Image(program.provider.image)
+                            .resizable()
+                            .frame(width: 32, height: 32)
                         Spacer()
                     }
-                    .padding(.horizontal)
-                }
-                
-                Spacer()
-            }
-            
-            HStack(alignment: .top) {
-                VStack(alignment: .leading) {
-                    Image(program.provider.image)
-                        .resizable()
-                        .frame(width: 32, height: 32)
                     Spacer()
                 }
-                Spacer()
+                
+                if geo.size.width < geo.size.height {
+                    VStack {
+                        ContentView(program: program, isPortrait: true)
+                    }
+                    .frame(width: geo.size.width)
+                    .background(Color("ProgramContentBackground"))
+                } else {
+                    HStack {
+                        ContentView(program: program, isPortrait: false)
+                    }
+                    .frame(width: geo.size.width)
+                    .background(Color("ProgramContentBackground"))
+                }
             }
         }.navigationTitle(program.title)
+    }
+}
+
+struct ContentView: View {
+    let program: Program
+    let isPortrait: Bool
+    
+    var body: some View {
+        Text(program.description)
+            .font(.body)
+            .foregroundColor(.primary)
+            .multilineTextAlignment(.leading)
+            .padding(.all)
+        
+        if !isPortrait {
+            Spacer()
+        }
+        
+        Button() {
+            openProgram()
+        } label: {
+            Label("Spela", systemImage: "play.fill").padding(.horizontal).font(.callout.bold())
+        }
+        .buttonStyle(.borderedProminent)
+        .tint(.accentColor)
+        .padding(.all)
+        
+        if isPortrait {
+            Spacer().frame(height: 20)
+        }
     }
     
     private func openProgram() {
@@ -72,7 +88,9 @@ struct ProgramView: View {
 struct ProgramView_Previews: PreviewProvider {
     static var previews: some View {
         ProgramView(program: .mock)
+            .preferredColorScheme(.dark)
             .previewInterfaceOrientation(.landscapeLeft)
     }
 }
+
 
